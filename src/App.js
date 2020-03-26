10995ca7e0d031f50b6ba0fe87678fc64f7a6709
Unai Bolivar <ujbolivar@gmail.com>
2020-03-26 20:44:45 +0100

    Working on router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 
-import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from "react-router-dom";
+
+import HomePage from "./pages/homepage/homepage.component";
 
 import "./App.css";
 
 function App() {
-  return <div>
-    <HomePage />
-  </div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
