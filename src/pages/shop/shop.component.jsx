df7d7e06ff21c0374acf17bdfb214751b8407bab
Unai Bolivar <ujbolivar@gmail.com>
2020-04-05 19:49:53 +0200

    Created collection preview component and inserted it into shop Page. Mapped through items in SHOP_DATA and displayed the titles

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 
 import SHOP_DATA from "./shop.data";
 
+import CollectionPreview from '../../components/preview-collection/collection-preview.component'
+
+
 class ShopPage extends Component {
   constructor(props) {
     super(props);
@@ -12,7 +15,14 @@ class ShopPage extends Component {
   }
 
   render() {
-    return <div>SHOP PAGE</div>;
+    const {collections} = this.state;
+    return (<div className='shop-page'>
+        {
+            collections.map(({id, ...otherCollectionProps}) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))
+        }
+    </div>);
   }
 }
 
