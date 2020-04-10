2682878ccc23f40e1aaf3e6f47922aa9ce7c90e4
Unai Bolivar <ujbolivar@gmail.com>
2020-04-10 11:42:52 +0200

    Created Sign-up component, added it to sign-in-and-sign-up page, enabled email sign up in firebase

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 
+
 class App extends React.Component {
   constructor() {
     super();
@@ -33,6 +34,8 @@ class App extends React.Component {
               ...snapShot.data(),
             },
           });
+
+          console.log(this.state)
         });
       }
       this.setState({ currentUser: userAuth});
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.components';
 
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUp = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
