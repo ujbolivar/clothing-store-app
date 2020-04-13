b3c5993d669aaf822d8b57adaf48bdc5262bb12a
Unai Bolivar <ujbolivar@gmail.com>
2020-04-13 20:58:48 +0200

    added cart icon and cart dropdown components to app, also implemented cart reducer

---
+++ src/components/cart/cart.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './cast.styles.scss';
+
+class Cart extends React.Component {
+    constructor() {
+        super();
+        
+        this.state = []
+    }
+    
+    render() {
+        return (
+            console.log("this is the cart")
+        )
+    }
+}
+
+export default Cart;
